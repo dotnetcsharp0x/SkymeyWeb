93dc3d62f66ba76637a53409144af27edc27f429
dotnetcsharp0x <dsotnikov1996@gmail.com>
2024-11-09 15:01:42 +0300

    Create token function

---
+++ SkymeyTokens/Controllers/TokenController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using SkymeyLibs.Data;
+using SkymeyLibs.Interfaces.Data;
+using SkymeyLibs.Models.Tables.Posts;
+using SkymeyLibs;
+using SkymeyLibs.Models.Tables.Tokens;
+
+namespace SkymeyTokens.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TokenController : Controller
+    {
+        private readonly IMongoRepository _db;
+        private readonly ILogger<TokenController> _logger;
+        private readonly IOptions<MainSettings> _options;
+
+        public TokenController(ILogger<TokenController> logger, IOptions<MainSettings> options,IMongoRepository db)
+        {
+            _logger = logger;
+            _options = options;
+            _db = db;
+        }
+
+        [HttpGet]
+        [Route("GetTokens")]
+        public async Task<IEnumerable<API_TOKEN>> GetTokens()
+        {
+            return await _db.GetTokens();
+        }
+
+        [HttpPost]
+        [Route("AddToken")]
+        public async Task<IActionResult> AddToken(API_TOKEN token)
+        {
+            await _db.AddToken(token);
+            return Ok();
+        }
+    }
+}
---
+++ SkymeyTokens/Program.cs
@@ -0,0 +1,31 @@
+using SkymeyLibs;
+using SkymeyLibs.Data;
+using SkymeyLibs.Interfaces.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.WebHost.UseUrls("http://localhost:5100;https://localhost:5101;");
+builder.Configuration.SetBasePath(builder.Configuration.GetSection("ConfigPath").Value)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+builder.Services.Configure<MainSettings>(builder.Configuration.GetSection("MainSettings"));
+builder.Services.AddSingleton<IMongoRepository, MongoPostRepository>();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
