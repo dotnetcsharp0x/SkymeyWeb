0d120c522b79f834175c81a682c93db84c746e04
dotnetcsharp0x <dsotnikov1996@gmail.com>
2024-11-19 16:53:22 +0300

    change

---
+++ SkymeyMainAPI/Controllers/TokenController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using SkymeyLibs.Models.Tables.Posts;
+using SkymeyLibs.Models.Tables.Tokens;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace SkymeyGatewayAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TokenController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetTokens")]
+        public async Task<IEnumerable<API_TOKEN>> GetTokens()
+        {
+            using (var client = new HttpClient())
+            {
+                IEnumerable<API_TOKEN> result = await client.GetFromJsonAsync<IEnumerable<API_TOKEN>>("https://localhost:5101/Token/GetTokens");
+                return result;
+            }
+        }
+        [HttpGet]
+        [Route("GetTokenList")]
+        public async Task<List<TokenList>> GetTokenList()
+        {
+            using (var client = new HttpClient())
+            {
+                List<TokenList> result = await client.GetFromJsonAsync<List<TokenList>>("https://localhost:5101/Token/GetTokenList");
+                return result;
+            }
+        }
+        [HttpPost]
+        [Route("AddToken")]
+        public async Task<bool> AddToken(API_TOKEN token)
+        {
+            using (var client = new HttpClient())
+            {
+                var content = new StringContent(JsonSerializer.Serialize(token), null, "application/json");
+
+                var resp = await client.PostAsync("https://localhost:5101/Token/AddToken", content);
+            }
+            return true;
+        }
+    }
+}
---
+++ SkymeyMainAPI/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//builder.WebHost.UseUrls("http://localhost:5070;https://localhost:5071;");
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+//
